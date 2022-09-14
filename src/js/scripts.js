d4967a2597857ea8b56fcbe8e547921fbfbaa118
shytuleva24 <shytuleva@gmail.com>
2022-09-14 13:13:25 +0300

    triple click

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,39 +1,38 @@
 // параллакс еффект
 let parallaxStartPos,
-    shiftX,
-    offsetX,
-    offsetY,
-    shiftY;
-let previousScroll = 0;
+    previousScroll = 0,
+    scrollOffsetY = 1;
 const parallaxBG = document.querySelectorAll(".parallax");
 const homePage = document.querySelector(".home-page");
 
-function moveBackground(e) {
-    let Y = e.pageY - window.pageYOffset - e.target.getBoundingClientRect().top + 1;
-    offsetX = 50 + (e.pageX / window.innerWidth * 20);
-    offsetY = 50 + (Y / window.innerHeight * 10);
-    e.target.style.backgroundPosition = `${offsetX}% ${offsetY}%`;
+function moveBackground(e) { // на комп
+    if (window.innerWidth > 1024) {
+        let Y = e.pageY - window.pageYOffset - e.target.getBoundingClientRect().top + 1;
+        let offsetX = 50 + (e.pageX / window.innerWidth * 20);
+        let offsetY = 50 + (Y / window.innerHeight * 10);
+        e.target.style.backgroundPosition = `${offsetX}% ${offsetY}%`;
+    }
 }
 
+window.addEventListener('scroll', function() { // на планшет
+    let currentScroll = window.pageYOffset;
+    if (window.pageYOffset < window.innerHeight && window.innerWidth < 1024) {
+        if (currentScroll > previousScroll && scrollOffsetY < 99){
+            scrollOffsetY += 0.2;
+            homePage.style.backgroundPosition = `50% ${scrollOffsetY}%`;
+        } else if (scrollOffsetY > 1) {
+            scrollOffsetY -= 0.2;
+            homePage.style.backgroundPosition = `50% ${scrollOffsetY}%`;
 
-// window.addEventListener('scroll', function() {
-//     let currentScroll = window.pageYOffset;
-//     if (window.pageYOffset < window.innerHeight) {
-//         if (currentScroll > previousScroll){
-//             offsetY += 0.7
-//         } else {
-//             offsetY -= 0.7
-//         }
-//         homePage.style.backgroundPosition = `${offsetX}% ${offsetY}%`;
-//     }
-//     previousScroll = currentScroll;
-// });
+        }
+    }
+    previousScroll = currentScroll;
+});
 
 parallaxBG.forEach(element => {
-    element.style.backgroundPosition = `center`;
-    element.style.backgroundSize = `130% auto`;
+    // element.style.backgroundPosition = `center`;
+    // element.style.backgroundSize = `130% auto`;
     element.addEventListener("mousemove", function (e) { moveBackground(e); });
-    
 });
 
 // let patern = /url\(["']?(.+)["']?\)/g;
@@ -57,7 +56,7 @@ if (animItems.length > 0) {
             if (animItemHeight > window.innerHeight) {
                 animItemPoint = window.innerHeight - window.innerHeight / animStart
             }
-            if ((window.pageYOffset > animItemOffSet - animItemPoint) && pageYOffset < (animItemOffSet + animItemHeight)) {
+            if ((window.pageYOffset > animItemOffSet - animItemPoint) && window.pageYOffset < (animItemOffSet + animItemHeight)) {
                 animItem.classList.add(`_active`)
             } else {
                 if (!(animItem.classList.contains(`_anim-no-hide`))) {
@@ -75,5 +74,92 @@ if (animItems.length > 0) {
 
     setTimeout(() => {
         animOnScroll()
-    }, 500)
+    }, 300)
 }
+// localStorage home page
+
+let titleHome = document.querySelector(".title-home"),
+    firstVisit,
+    lastVisit,
+    timeInSite;
+
+window.onload = function () {
+    if (!localStorage.firstVisit || localStorage.firstVisit == "undefined") {
+        localStorage.firstVisit = new Date ();
+        titleHome.innerText = "Welcome to STARTUP";
+    }
+    firstVisit = localStorage.firstVisit;
+    
+    if (!localStorage.lastVisit) {
+        localStorage.lastVisit = firstVisit;
+    } 
+    timeInSite = (new Date () - new Date (localStorage.lastVisit)) / 1000 / 60; // min
+    let timeInSiteFirst = (new Date () - new Date (localStorage.firstVisit)) / 1000 / 60; // min
+    if (timeInSiteFirst <= 5) {
+        titleHome.innerText = "Welcome to STARTUP";
+    } else if (timeInSite < 5) {
+        titleHome.innerText = "glad to see you again";
+    } else if (timeInSite >= 1440) {
+        titleHome.innerText = "you were gone for a day, welcome to STARTUP";
+    } else if (timeInSite >= 5) {
+        titleHome.innerText = "welcome back to STARTUP";
+    } 
+
+    window.onblur = function() {
+        localStorage.lastVisit = new Date ();
+    }
+    lastVisit = localStorage.lastVisit;
+}
+
+// tripl click
+
+const tripleClick = document.querySelector(".rock-solid svg")
+const changeClicks = document.querySelectorAll(".services-card h3")
+let timer;  
+tripleClick.addEventListener("dblclick", function () {
+    timer = setTimeout(function () {
+        timer = null;
+    }, 200);
+});
+tripleClick.addEventListener("click", function () {
+    if (timer) {
+        clearTimeout(timer);
+        timer = null;
+        changeInClick();
+    }
+});
+function changeInClick () {
+    changeClicks.forEach(element => {
+        element.innerText = "Yeah, you did triple-click!!!"
+    });
+}
+
+// slider
+
+const prevBtn = document.querySelector('.cards-arrow.left')
+const nextBtn = document.querySelector('.cards-arrow.right')
+const outImg = document.querySelector('.slider-cards')
+const aboutCard = document.querySelectorAll('.about-card')
+
+// let i = 0
+// outImg.innerHTML = aboutCard
+
+
+// nextBtn.onclick = () => {
+//     if (i < aboutCard.length - 1){
+//         i++
+//     } else {
+//         i = 0
+//     }
+//     outImg.innerHTML = aboutCard[i] 
+// }
+
+// prevBtn.onclick = () => {
+//     if (i > 0 ){
+//         i--
+//     } else {
+//         i = aboutCard.length - 1 
+//     }
+//     outImg.innerHTML = aboutCard[i] 
+// }
+
